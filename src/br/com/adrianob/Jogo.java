0f4114037439c8a7703f1621289be7f6208defc6
adriano barbosa <b.adrianobarbosa@gmail.com>
2016-07-02 09:26:52 -0300

    Hora do Café!!!

--- src/br/com/adrianob/Jogo.java
+++ src/br/com/adrianob/Jogo.java
@@ -1,19 +1,59 @@
-package br.com.adrianob;
+package br.com.munif.alfa.jogo;
 
-/**
- *
- * @author Administrador
- */
+import javax.swing.JOptionPane;
+
 public class Jogo {
     
-    public static void main(String[] args) {
-        String nome;
-        if (args.length==0) {
-            nome = "Mundo";
-        } else {
-            nome = args[0];
-        }
-        System.out.println("Olá "+nome+"!!!");
+    public static void main(String [] args){
+        Personagem matheus=new Personagem("Matheus");
+        
+        
+          
+        System.out.println(matheus);
+        JOptionPane.showMessageDialog(null, matheus);
+        
+
+        Personagem renan=new Personagem("Renan");
+        
+        
+          
+        
+
         
     }
+}
+
+
+
+/**
+    Esta é a classe Personagem que serve para 
+*/
+class Personagem{
+    
+    String nome; 
+    /**
+     * Se chegar a zero morreu
+     */
+    int energia;
+
+    public Personagem() {
+        System.out.println("Executando o construtor sem parâmetros de Personagem");
+        nome="Sem nome";
+        energia=100;
+    }
+    public Personagem(String nome) {
+        System.out.println("Executando o construtor sem parâmetros de Personagem");
+        this.nome=nome;
+        energia=100;
+    }
+    
+    
+    public String toString(){
+        String aRetornar;
+        aRetornar=this.nome+" "+this.energia;
+        return aRetornar;
+    }
+    
+    
+    
 }
